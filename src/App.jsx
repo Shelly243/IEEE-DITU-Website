9ca8814f774104abb925800461d1a17740b4ad7b
shreyal0007 <shreyaljain0007@gmail.com>
2023-07-14 17:15:32 +0530

    navbar completed

--- src/App.jsx
+++ src/App.jsx
@@ -9,17 +9,21 @@ import AboutPage from "./pages/about/AboutPage";
 import TeamsPage from "./pages/teams/TeamsPage";
 import EventsPage from "./pages/events/EventsPage";
 import Contact from "./pages/contact/Contact";
+import Navbar from "./Components/Navbar/Navbar";
+import Gallery from "./pages/gallery/Gallery";
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Navbar />
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/about" element={<AboutPage />} />
           <Route path="/teams" element={<TeamsPage />} />
           <Route path="/events" element={<EventsPage />} />
           <Route path="/contact" element={<Contact />} />
+          <Route path="/gallery" element={<Gallery/>} />
           <Route path="*" element={<HomePage />} />
         </Routes>
       </Router>
