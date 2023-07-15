c5b78dba957078e44ca47c2adfc7bcccb9dd4d86
Diya Lohani <shellylohani2018@gmail.com>
2023-07-15 15:17:45 +0530

    added cards and card-slider component to eventspage

---
+++ src/pages/events/CardSlider.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import '../events/EventsPage.css'
+import EventsHoriCard from './EventsHoriCard';
+
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+
+function CardSlider() {
+  const responsive = {
+  superLargeDesktop: {
+    // the naming can be any, depends on you.
+    breakpoint: { max: 4000, min: 3000 },
+    items: 6
+  },
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 4
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 2
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1
+  }
+};
+  return (
+    <div>
+        <Carousel responsive={responsive}>
+            <div><EventsHoriCard /></div>
+            <div><EventsHoriCard /></div>
+            <div><EventsHoriCard /></div>
+            <div><EventsHoriCard /></div>
+            <div><EventsHoriCard /></div>
+            <div><EventsHoriCard /></div>
+            <div><EventsHoriCard /></div> 
+        </Carousel>
+    </div>
+  )
+}
+
+export default CardSlider
---
+++ src/pages/events/EventsHoriCard.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import '../events/EventsPage.css'
+
+function EventsHoriCard() {
+  return (
+    <div>
+        {/*cards */}
+        <div className="hori-card">
+            {/*image*/}
+            <div className="image">
+                <img src="https://picsum.photos/200/300" alt="image" />
+            </div>
+            {/*description*/}
+            <div className="des">
+                <h3>title Here</h3>
+                <br />
+                <h5>Coordinator</h5>
+                <h5>Speaker</h5>
+                <h5>Date</h5>
+                <br />
+                {/*button*/}
+                <a href='_blank' className="hori-card-btn">Read More</a>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default EventsHoriCard
