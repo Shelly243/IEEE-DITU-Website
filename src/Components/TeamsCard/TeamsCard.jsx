3cf2a6b2921e654356020226df5fa6fa39af20a6
Vaibhav Aswal <vbhv.aswal@gmail.com>
2023-07-23 18:04:57 +0530

    finished teams page

--- src/Components/TeamsCard/TeamsCard.jsx
+++ src/Components/TeamsCard/TeamsCard.jsx
@@ -1,5 +1,35 @@
 import "./TeamsCard.css";
+import git from "../../assets/github.png";
+import linkedi from "../../assets/linkedin.png";
 
-export const TeamsCard = () => {
-  return <div>TeamsCard</div>;
+export const TeamsCard = ({
+  profileUrl,
+  name,
+  description,
+  github,
+  linkedin,
+}) => {
+  return (
+    <div className="teamsCard">
+      <div className="teamsCardUpper">
+        <img src={profileUrl} alt={name} className="user" />
+      </div>
+      <div className="teamsCardLower">
+        <h3 className="name">{name}</h3>
+        <p className="desc">{description}</p>
+        <div className="teamsSocials">
+          {github && (
+            <a href={github} target="_blank">
+              <img src={git} alt="github" />
+            </a>
+          )}
+          {linkedin && (
+            <a href={linkedin} target="_blank">
+              <img src={linkedi} alt="linkedin" />
+            </a>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 };
